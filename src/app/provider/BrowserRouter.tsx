e3575e30b2d7b2b8b2bf591a35f0e8734637cc8b
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-10 15:19:17 +0300

    removed: deleted about page

--- src/app/provider/BrowserRouter.tsx
+++ src/app/provider/BrowserRouter.tsx
@@ -7,21 +7,18 @@ import {
 import {Layout} from "pages/LayoutPage/Layout";
 import {NotFoundPage} from "pages/NotFound";
 import {HomePage} from "pages/HomePage/HomePage";
-import {AboutPage} from "pages/AboutPage/AboutPage";
 import {RegulationsPage} from "pages/RegulationsPage/RegulationsPage";
 import {DepartmentsPage, departmentsLoader} from "pages/DepartmentsPage/DepartmentsPage";
 import {ContactsPage} from "pages/ContactsPage/ContactsPage";
 
-import {DepartmentList, DepartmentSingle} from "components/Department";
+import {DepartmentList} from "components/Department";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout/>} errorElement={<NotFoundPage/>}>
         <Route index element={<HomePage/>}/>
-        <Route path="about" element={<AboutPage/>}/>
         <Route path="regulations" element={<RegulationsPage/>}/>
         <Route path="departments" element={<DepartmentsPage/>}>
             <Route index element={<DepartmentList/>} loader={departmentsLoader}/>
-            <Route path=":departmentId" element={<DepartmentSingle/>}/>
         </Route>
         <Route path="contacts" element={<ContactsPage/>}/>
     </Route>
