a80a429c73153258b3a4bfaaa166e1d42b40929e
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-14 11:48:03 +0300

    feat: added login link and logout btn

--- src/components/LayoutComponents/Header/Header.tsx
+++ src/components/LayoutComponents/Header/Header.tsx
@@ -1,11 +1,17 @@
+import {NavLink, useNavigate} from "react-router-dom";
 import {FC, useEffect, useState} from "react";
+
+import {useAuth} from "app/provider/AuthProvider";
+import {HamburgerMenu} from "../HamburgerMenu/HamburgerMenu";
 import {NavigationList} from "../NavigationList/NavigationList";
 import {Logo} from "../Logo/Logo";
-import {HamburgerMenu} from "../HamburgerMenu/HamburgerMenu";
 import {Logo as LogoImage} from "assets/media/images";
+
 import "./Header.scss";
 
 const Header: FC = () => {
+    const navigate = useNavigate();
+    const {user, logout} = useAuth();
     const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
 
     const isMobile = windowWidth < 768;
@@ -34,7 +40,22 @@ const Header: FC = () => {
                         listClassName="nav-list"
                         listItemClassName="nav-list__element"
                         linkClassName="nav-list__link"
-                    />
+                    >
+                        <li className="nav-list__element">
+                            {!user ?
+                                <NavLink className="nav-list__link" to="/login">
+                                    Увійти
+                                </NavLink>
+                                :
+                                <button className="nav-list__btn" onClick={() => {
+                                    logout();
+                                    navigate("/");
+                                }}>
+                                    Вийти
+                                </button>
+                            }
+                        </li>
+                    </NavigationList>
                 </nav>
             ) : (
                 <HamburgerMenu/>
