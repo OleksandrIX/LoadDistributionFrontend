11af4d5bcc11d427f3184f159ecc4ced8b1f028a
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-15 10:10:35 +0300

    feat: created NotFound Page

--- src/pages/ErrorPage/index.ts
+++ src/pages/ErrorPage/index.ts
@@ -1,5 +1,5 @@
 import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 import ForbiddenPage from "./Forbidden/ForbiddenPage";
-import {NotFoundPage} from "./NotFound";
+import NotFoundPage from "./NotFound/NotFoundPage/NotFoundPage";
 
 export {ErrorBoundary, ForbiddenPage, NotFoundPage};
