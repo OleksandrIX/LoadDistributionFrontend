6a10b94ce487b436cc6e28caf095fc7405d0027b
Олександр Загоренко <93474880+OleksandrIX@users.noreply.github.com>
2024-05-15 16:44:52 +0300

    Removed mobile styles (#6)
    * fix: removed mobile styles
    
    * fix: removed hamburger menu

--- src/components/LayoutComponents/Header/Header.tsx
+++ src/components/LayoutComponents/Header/Header.tsx
@@ -1,8 +1,7 @@
+import {FC} from "react";
 import {NavLink, useNavigate} from "react-router-dom";
-import {FC, useEffect, useState} from "react";
 
 import {useAuth} from "app/provider/AuthProvider";
-import {HamburgerMenu} from "../HamburgerMenu/HamburgerMenu";
 import {NavigationList} from "../NavigationList/NavigationList";
 import {Logo} from "../Logo/Logo";
 import {Logo as LogoImage} from "assets/media/images";
@@ -12,15 +11,6 @@ import "./Header.scss";
 const Header: FC = () => {
     const navigate = useNavigate();
     const {user, logout} = useAuth();
-    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
-
-    const isMobile = windowWidth < 768;
-    const handleResize = () => setWindowWidth(window.innerWidth);
-
-    useEffect(() => {
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
 
     return (
         <header className="page__header">
@@ -34,32 +24,28 @@ const Header: FC = () => {
                 </p>
             </Logo>
 
-            {!isMobile ? (
-                <nav className="header__navigation">
-                    <NavigationList
-                        listClassName="nav-list"
-                        listItemClassName="nav-list__element"
-                        linkClassName="nav-list__link"
-                    >
-                        <li className="nav-list__element">
-                            {!user ?
-                                <NavLink className="nav-list__link" to="/login">
-                                    Увійти
-                                </NavLink>
-                                :
-                                <button className="nav-list__btn" onClick={() => {
-                                    logout();
-                                    navigate("/");
-                                }}>
-                                    Вийти
-                                </button>
-                            }
-                        </li>
-                    </NavigationList>
-                </nav>
-            ) : (
-                <HamburgerMenu/>
-            )}
+            <nav className="header__navigation">
+                <NavigationList
+                    listClassName="nav-list"
+                    listItemClassName="nav-list__element"
+                    linkClassName="nav-list__link"
+                >
+                    <li className="nav-list__element">
+                        {!user ?
+                            <NavLink className="nav-list__link" to="/login">
+                                Увійти
+                            </NavLink>
+                            :
+                            <button className="nav-list__btn" onClick={() => {
+                                logout();
+                                navigate("/");
+                            }}>
+                                Вийти
+                            </button>
+                        }
+                    </li>
+                </NavigationList>
+            </nav>
         </header>
     );
 };
