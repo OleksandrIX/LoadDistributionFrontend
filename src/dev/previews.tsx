b6f5339769112b26645efd835d04a8a4b5b93eca
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-18 20:11:41 +0300

    fix: updated dev folder

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,15 +1,12 @@
 import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
 import {PaletteTree} from "./palette";
-import TeacherList from "../components/Teacher/TeacherList/TeacherList";
+import TeacherList from "../components/TeacherComponents/TeacherList/TeacherList";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/ComponentPreviews">
                 <ComponentPreviews/>
-            </ComponentPreview>
-            <ComponentPreview path="/TeacherList">
-                <TeacherList/>
             </ComponentPreview>
         </Previews>
     );
