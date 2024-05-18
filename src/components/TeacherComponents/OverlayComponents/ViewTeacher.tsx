74854d39c847f71f53eb4ff540503796242d3d63
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-18 20:17:45 +0300

    feat: created overlay components for teacher (view, create, edit and delete)

---
+++ src/components/TeacherComponents/OverlayComponents/ViewTeacher.tsx
@@ -0,0 +1,35 @@
+import {FC} from "react";
+import {
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalHeader,
+    ModalOverlay
+} from "@chakra-ui/modal";
+
+import TeacherCard from "../TeacherCard/TeacherCard";
+import {Teacher} from "entities/teacher";
+
+interface ViewTeacherProps {
+    isOpen: boolean;
+    onClose: () => void;
+    teacher: Teacher;
+}
+
+const ViewTeacher: FC<ViewTeacherProps> = ({isOpen, onClose, teacher}) => {
+    return (
+        <Modal isOpen={isOpen} onClose={onClose} size="xl">
+            <ModalOverlay/>
+            <ModalContent>
+                <ModalHeader>Інформація про викладача</ModalHeader>
+                <ModalCloseButton/>
+                <ModalBody>
+                    <TeacherCard teacher={teacher}/>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    );
+};
+
+export default ViewTeacher;
