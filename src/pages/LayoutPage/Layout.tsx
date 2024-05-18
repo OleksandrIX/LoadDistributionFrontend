8f44ded11eaed0f6d13d0c1e04a4d40fbd814288
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-18 20:13:54 +0300

    fix: fixed styles for layout page and added loader

--- src/pages/LayoutPage/Layout.tsx
+++ src/pages/LayoutPage/Layout.tsx
@@ -1,40 +1,33 @@
 import {FC} from "react";
 import {Outlet} from "react-router-dom";
-import {Spinner} from "@chakra-ui/react";
 
-import {Header} from "components/LayoutComponents/Header/Header";
-import {Footer} from "components/LayoutComponents/Footer/Footer";
-import {useAuth} from "app/provider/AuthProvider";
+import {useAuth} from "app/provider";
+import {Loader} from "components/UI";
+import {Header, Footer} from "components/LayoutComponents";
+
 import "./Layout.scss";
 
 const Layout: FC = () => {
     const {isLoading} = useAuth();
 
     return (
-        <>
+        <div className="page-container">
+
             {
-                isLoading ?
-                    <div className="page-loader">
-                        <Spinner
-                            thickness="4px"
-                            speed="0.65s"
-                            emptyColor="gray.200"
-                            color="blue.500"
-                            size="xl"
-                        />
+                isLoading
+                    ? <div className="loader-wrapper">
+                        <Loader/>
                     </div>
-                    :
-                    <div className="page-container">
+                    : <>
                         <Header/>
                         <main className="page__body">
                             <Outlet/>
                         </main>
                         <Footer/>
-                    </div>
+                    </>
             }
-        </>
+        </div>
     );
-
 };
 
 export default Layout;
