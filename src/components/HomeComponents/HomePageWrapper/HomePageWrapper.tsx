4d007b808c0ef3f7fb784268938ea17566d91364
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-19 18:10:53 +0300

    feat: changed CurrentUser and AdminUser components on HomePageWrapper component

---
+++ src/components/HomeComponents/HomePageWrapper/HomePageWrapper.tsx
@@ -0,0 +1,36 @@
+import {FC} from "react";
+import {Box, Stack} from "@chakra-ui/react";
+
+import {useAuth, useSidebar} from "app/provider";
+import {Sidebar, sidebarAdminElements, sidebarUserElements} from "../Sidebar";
+
+const HomePageWrapper: FC = () => {
+    const {isAdmin} = useAuth();
+    const {selectedElement} = useSidebar();
+
+    const getSelectedComponent = () => {
+        let selectedSidebarElement;
+        if (isAdmin) {
+            selectedSidebarElement = sidebarAdminElements.find(element =>
+                element.sidebarElementType === selectedElement);
+        } else {
+            selectedSidebarElement = sidebarUserElements.find(element =>
+                element.sidebarElementType === selectedElement);
+        }
+        return selectedSidebarElement ? selectedSidebarElement.children : <h1>UNKNOWN</h1>;
+    };
+    return (
+        <Stack
+            gap={5}
+            direction="row"
+            minHeight="90vh"
+        >
+            <Sidebar/>
+            <Box flex={1} p="10px">
+                {getSelectedComponent()}
+            </Box>
+        </Stack>
+    );
+};
+
+export default HomePageWrapper;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,18 @@
 import {FC} from "react";
 
 import {SidebarProvider, useAuth} from "app/provider";
-import {CurrentUser, Intro} from "components/HomeComponents";
+import {HomePageWrapper, Intro} from "components/HomeComponents";
 
 import "./HomePage.scss";
 
 
 const HomePage: FC = () => {
-    const {user, isAdmin} = useAuth();
-
+    const {user} = useAuth();
 
     return (
         <SidebarProvider>
             <div className="home-page">
-                {user ? !isAdmin ? <CurrentUser/> : <h1>ADMIN</h1> : <Intro/>}
+                {user ? <HomePageWrapper/> : <Intro/>}
             </div>
         </SidebarProvider>
     );
