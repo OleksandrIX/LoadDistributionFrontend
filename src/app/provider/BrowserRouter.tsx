f8c6ffd9a17da64da1ced35e3b7494b0feb5a25b
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-20 11:24:30 +0300

    fix: removed department page from browser router

--- src/app/provider/BrowserRouter.tsx
+++ src/app/provider/BrowserRouter.tsx
@@ -1,24 +1,12 @@
-import {FC} from "react";
-import {createBrowserRouter, createRoutesFromElements, Outlet, Route} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
 
-import {useAuth} from "./AuthProvider";
 import Layout from "pages/LayoutPage/Layout";
 import HomePage from "pages/HomePage/HomePage";
 import RegulationsPage from "pages/RegulationsPage/RegulationsPage";
-import DepartmentsPage, {departmentsLoader} from "pages/DepartmentsPage/DepartmentsPage";
 import ContactsPage from "pages/ContactsPage/ContactsPage";
 import LoginPage from "pages/LoginPage/LoginPage";
 import RegistrationPage from "pages/RegistrationPage/RegistrationPage";
-import {ErrorBoundary, ForbiddenPage, NotFoundPage} from "pages/ErrorPage";
-import {DepartmentList} from "components/DepartmentComponents";
-
-
-const ProtectedRoutes: FC = () => {
-    const {user} = useAuth();
-    if (!user)
-        return <ForbiddenPage/>;
-    return <Outlet/>;
-};
+import {ErrorBoundary, NotFoundPage} from "pages/ErrorPage";
 
 
 const router = createBrowserRouter(createRoutesFromElements(
@@ -28,11 +16,6 @@ const router = createBrowserRouter(createRoutesFromElements(
         <Route path="contacts" element={<ContactsPage/>} errorElement={<ErrorBoundary/>}/>
         <Route path="login" element={<LoginPage/>} errorElement={<ErrorBoundary/>}/>
         <Route path="registration" element={<RegistrationPage/>}/>
-        <Route element={<ProtectedRoutes/>} errorElement={<ErrorBoundary/>}>
-            <Route path="departments" element={<DepartmentsPage/>}>
-                <Route index element={<DepartmentList/>} loader={departmentsLoader}/>
-            </Route>
-        </Route>
         <Route path="*" element={<NotFoundPage/>}/>
     </Route>
 ));
