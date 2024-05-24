accefe0b3e8b43c638cae4f6c688a6e2216da19b
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-24 13:25:57 +0300

    feat: init discipline components

---
+++ src/components/DisciplineComponents/DisciplineWrapper/DisciplineWrapper.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+
+const DisciplineWrapper: FC = () => {
+    return (
+        <div>
+            Дисципліни
+        </div>
+    );
+};
+
+export default DisciplineWrapper;
