f7930a02629b47f0a770109d48ac433fd7f191f2
OleksandrIX <zagorenkosasha@gmail.com>
2024-06-07 11:53:08 +0300

    feat: added select for selecting data of years for academic workload

--- src/components/WorkloadComponents/WorkloadDisciplineComponent/DisciplinesList.tsx
+++ src/components/WorkloadComponents/WorkloadDisciplineComponent/DisciplinesList.tsx
@@ -1,27 +1,19 @@
 import {FC} from "react";
 import {Stack} from "@chakra-ui/react";
-import {ResponseAcademicWorkload, ResponseDiscipline} from "entities/discipline";
+import {ResponseDiscipline} from "entities/discipline";
 import DisciplineListItem from "./DisciplineListItem";
 
 interface DisciplinesListProps {
     disciplines: ResponseDiscipline[]
 }
 
-const getTotalWorkloadHours = (workload: ResponseAcademicWorkload) =>
-    Object.values(workload).reduce((acc, hours) => typeof hours === "number" ? acc + hours : acc, 0);
+// const getTotalWorkloadHours = (workload: ResponseAcademicWorkload) =>
+//     Object.values(workload).reduce((acc, hours) => typeof hours === "number" ? acc + hours : acc, 0);
 
 const DisciplinesList: FC<DisciplinesListProps> = ({disciplines}) => {
     return (
         <Stack spacing={4}>
-            {
-                disciplines
-                    .sort((a, b) =>
-                        getTotalWorkloadHours(b.academic_workload) - getTotalWorkloadHours(a.academic_workload)
-                    )
-                    .map((discipline) =>
-                        <DisciplineListItem key={discipline.id} discipline={discipline}/>
-                    )
-            }
+            {disciplines.map((discipline) => <DisciplineListItem key={discipline.id} discipline={discipline}/>)}
         </Stack>
     );
 };
--- src/components/WorkloadComponents/WorkloadWrapper/WorkloadWrapper.tsx
+++ src/components/WorkloadComponents/WorkloadWrapper/WorkloadWrapper.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import {FC, useCallback, useEffect, useState} from "react";
-import {Box, Heading, Stack, useToast} from "@chakra-ui/react";
+import {ChangeEvent, FC, useCallback, useEffect, useState} from "react";
+import {Box, Flex, Heading, Select, Stack, useToast} from "@chakra-ui/react";
 
 import {useAuth} from "app/provider";
 import {handleAxiosError} from "utils/error.handlers";
@@ -19,6 +19,9 @@ const WorkloadWrapper: FC = () => {
     const [disciplines, setDisciplines] = useState<ResponseDiscipline[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
 
+    const [dataOfYearsArray, setDataOfYearsArray] = useState<string[]>([]);
+    const [selectedDataOfYears, setSelectedDataOfYears] = useState<string>("");
+
     const fetchData = useCallback(async () => {
         const departmentService = new DepartmentService();
         const disciplineService = new DisciplineService();
@@ -26,6 +29,21 @@ const WorkloadWrapper: FC = () => {
             if (department) {
                 const responseDepartment = await departmentService.getDepartmentByIdWithTeachers(department.id);
                 const responseDiscipline = await disciplineService.getAllDisciplines();
+
+                const possibleDataOfYears: string[] = [];
+                for (const discipline of responseDiscipline) {
+                    !possibleDataOfYears.includes(discipline.data_of_years)
+                    && possibleDataOfYears.push(discipline.data_of_years);
+                }
+
+                possibleDataOfYears.sort((a, b) => {
+                    const startYear = parseInt(a.split("-")[0]);
+                    const endYear = parseInt(b.split("-")[1]);
+                    return startYear - endYear;
+                });
+
+                setDataOfYearsArray(possibleDataOfYears);
+                setSelectedDataOfYears(possibleDataOfYears[0]);
                 setTeachers(responseDepartment.teachers);
                 setDisciplines(responseDiscipline);
             }
@@ -44,6 +62,9 @@ const WorkloadWrapper: FC = () => {
             }
         }
     }, [department, refreshToken, workloadToast]);
+
+    const handleSelectDataOfYears = (event: ChangeEvent<HTMLSelectElement>) =>
+        setSelectedDataOfYears(event.target.value);
 
     useEffect(() => {
         fetchData().finally(() => setIsLoading(false));
@@ -82,10 +103,24 @@ const WorkloadWrapper: FC = () => {
 
     console.log(teachers);
     return (
-        <Stack>
-            <Heading mb={5} size="md" textAlign="center">Розподіл навчального навантаження</Heading>
+        <Stack spacing={4}>
+            <Flex my={2} align="center">
+                <Select w="fit-content" defaultValue={selectedDataOfYears} onChange={handleSelectDataOfYears}>
+                    {dataOfYearsArray.map((dataOfYear) =>
+                        <option key={dataOfYear} value={dataOfYear}>{dataOfYear}</option>
+                    )}
+                </Select>
 
-            <DisciplinesList disciplines={disciplines}/>
+                <Heading size="md" textAlign="center" flex={1}>
+                    Розподіл навчального навантаження
+                </Heading>
+            </Flex>
+
+            <DisciplinesList
+                disciplines={disciplines.filter((discipline) =>
+                    discipline.data_of_years === selectedDataOfYears && discipline
+                )}
+            />
         </Stack>
     );
 };
